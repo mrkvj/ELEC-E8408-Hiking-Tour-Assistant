a39da2c568aca02fc6b2aa160cc0f04b849098f3
Veikko Markkula <veikko.markkula@aalto.fi>
2025-03-15 13:24:23 +0200

    Code cleaned

--- RPi/wserver.py
+++ RPi/wserver.py
@@ -8,12 +8,19 @@ app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+################################################################
+# Chose to login or signup
 @app.route('/')
 def home():
-    return render_template('home.html')
 
+    return render_template('home.html')
+################################################################
+
+################################################################
+# Login option for existing users
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
     if request.method == 'POST':
         name = request.form['Username']
         password = request.form['Password']
@@ -21,7 +28,7 @@ def login():
         conn = get_db_connection()
         cursor = conn.cursor()
 
-        # Check if the username and password match an existing user
+        # Check if user and password pair exists
         cursor.execute("SELECT userID FROM user_info WHERE name = ? AND password = ?", (name, password))
         user = cursor.fetchone()
         conn.close()
@@ -34,26 +41,30 @@ def login():
             return redirect(url_for('login'))
 
     return render_template('login.html')
+################################################################
 
+################################################################
+# Signup page for new users
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+
     if request.method == 'POST':
         name = request.form['Name']
         password = request.form['Password']
         weight = int(request.form['Weight'])
-        role = 'admin'  # Assuming role is always 'admin' for now
+        role = 'admin'  # Admin as a default for now
 
         conn = get_db_connection()
         cursor = conn.cursor()
 
-        # Check if the username already exists
+        # Check if username exists
         cursor.execute("SELECT * FROM user_info WHERE name = ?", (name,))
         existing_user = cursor.fetchone()
 
         if existing_user:
             return "Username already exists. Please choose a different username."
 
-        # Generate a new user ID
+        # Generate user ID
         cursor.execute("SELECT MAX(userID) FROM user_info")
         max_id = cursor.fetchone()[0]
         new_user_id = (max_id if max_id is not None else 0) + 1
@@ -68,34 +79,41 @@ def signup():
 
     return render_template('signup.html')
 
+################################################################
+# Backend for user homepage
 @app.route('/login/<int:user_id>/')
 def user_homepage(user_id):
+
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    # Retrieve user information
+    # Get user information
     cursor.execute("SELECT * FROM user_info WHERE userID = ?", (user_id,))
     user_info = cursor.fetchone()
 
-    # Count the number of sessions for the user
+    # User session count
     cursor.execute("SELECT COUNT(*) as session_count FROM Session WHERE userID = ?", (user_id,))
     session_count = cursor.fetchone()['session_count']
 
-    # Retrieve the list of sessions for the user
+    # List of sessions
     cursor.execute("SELECT sessionID, steps, calories FROM Session WHERE userID = ?", (user_id,))
     sessions = cursor.fetchall()
 
     conn.close()
 
     return render_template('user_homepage.html', user_id=user_id, user_info=user_info, session_count=session_count, sessions=sessions)
+################################################################
 
 
+################################################################
+# Backend for session info page
 @app.route('/login/<int:user_id>/<int:session_id>')
 def session_info(user_id, session_id):
+
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    # Retrieve session information
+    # Get session information
     cursor.execute("SELECT * FROM Session WHERE sessionID = ? AND userID = ?", (session_id, user_id))
     session = cursor.fetchone()
 
@@ -103,31 +121,40 @@ def session_info(user_id, session_id):
 
     return render_template('session.html', user_id=user_id, session=session)
 
+################################################################
+# Backend for deleting a session
 @app.route('/login/<int:user_id>/<int:session_id>/delete', methods=['POST'])
 def delete_session(user_id, session_id):
+
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    # Delete the session from the database
+    # Delete curernt session from the database
     cursor.execute("DELETE FROM Session WHERE sessionID = ? AND userID = ?", (session_id, user_id))
     conn.commit()
     conn.close()
 
     print(f'DELETED SESSION WITH ID: {session_id}')
     return redirect(url_for('user_homepage', user_id=user_id))
+################################################################
 
+################################################################
 @socketio.on('connect')
 def handle_connect():
+
     print('Client connected')
 
 @socketio.on('disconnect')
 def handle_disconnect():
+
     print('Client disconnected')
 
 @socketio.on('message')
 def handle_message(msg):
+
     print('Message: ' + msg)
     emit('response', {'data': 'Message received'})
+################################################################
 
 if __name__ == "__main__":
     socketio.run(app, host='0.0.0.0', port=5001, debug=True)
